4f53bee5df84d73f6bb2714dee2529b7dc70a24d
gonza <gonzacastro99@gmail.com>
2021-06-14 03:45:26 -0300

    se agrego hilo en segundo plano que cuenta el tiempo y pide refrescar el token, todavia falta implementar el llamado a la api

--- app/src/main/java/com/example/GUI/HomeMenuActivity.java
+++ app/src/main/java/com/example/GUI/HomeMenuActivity.java
@@ -1,10 +1,13 @@
 package com.example.GUI;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -15,18 +18,25 @@ import Utils.Acelerometro;
 public class HomeMenuActivity extends AppCompatActivity {
 
     WebView miVisorWeb;
+    Handler hand;
     //public static Activity home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //home = this;
+        hand = new Handler();
         SensorManager a = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
         Acelerometro speedWagon = new Acelerometro();
         speedWagon.setSensorManager(a);
         speedWagon.setShake(this.getApplicationContext());
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_home_menu);
+
+        Runnable crono = new Cronometro(this.hand);
+        Thread t = new Thread(crono);
+        t.start();
+
         miVisorWeb = (WebView) findViewById(R.id.visorWeb);
         final WebSettings ajustesVisorWeb = miVisorWeb.getSettings();
         ajustesVisorWeb.setJavaScriptEnabled(true);
@@ -50,6 +60,47 @@ public class HomeMenuActivity extends AppCompatActivity {
             miVisorWeb.goBack();
         }else{
             super.onBackPressed();
+        }
+    }
+
+    class Cronometro implements Runnable {
+
+        Handler h;
+
+        public Cronometro(Handler h) {
+            this.h = h;
+        }
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            h.post(new Runnable() {
+                @Override
+                public void run() {
+                    new AlertDialog.Builder(HomeMenuActivity.this)
+                            .setTitle("Sesion expirada")
+                            .setMessage("Para continuar navegando en la aplicacion es necesario refrescar el token")
+                            .setPositiveButton("Refrescar", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                    Runnable r = new Cronometro(h);
+                                    Thread t = new Thread(r);
+                                    t.start();
+                                }
+                            })
+                            .setNegativeButton("Salir", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    finish();
+                                }
+                            })
+                            .show();
+                }
+            });
         }
     }
 
